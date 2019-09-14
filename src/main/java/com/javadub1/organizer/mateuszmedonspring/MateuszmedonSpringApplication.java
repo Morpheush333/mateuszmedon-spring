eae5ce41b6875ef3ae8088635d87f96b90b89679
Mateusz Medon <mateuszmedon1@gmail.com>
2019-09-14 10:07:22 +0100

    initial commit

---
+++ src/main/java/com/javadub1/organizer/mateuszmedonspring/MateuszmedonSpringApplication.java
@@ -0,0 +1,13 @@
+package com.javadub1.organizer.mateuszmedonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateuszmedonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateuszmedonSpringApplication.class, args);
+	}
+
+}
