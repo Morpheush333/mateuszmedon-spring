47a8a9b5a661dfb549c0f039cca1c06b6d6eda19
Mateusz Medon <mateuszmedon1@gmail.com>
2019-09-14 15:15:47 +0100

    Add User Configuration Class

--- src/main/java/com/javadub1/organizer/mateuszmedonspring/user/UserConfiguration.java
+++ src/main/java/com/javadub1/organizer/mateuszmedonspring/user/UserConfiguration.java
@@ -1,4 +1,18 @@
 package com.javadub1.organizer.mateuszmedonspring.user;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
 public class UserConfiguration {
+
+    @Bean
+    public UserRepository preInitializedInMemoryUserRepository() {
+        return new InMemoryUserRepository(Arrays.asList(
+                new User(1L,"Mariusz", "Golonko", Gender.LGBT)
+        ));
+    }
 }
