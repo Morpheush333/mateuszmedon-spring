475df21d98ee6ce427d4c22336275b44efcc1787
Mateusz Medon <mateuszmedon1@gmail.com>
2019-09-15 15:18:03 +0100

    example with PathVariable

--- src/main/java/com/javadub1/organizer/mateuszmedonspring/user/UserController.java
+++ src/main/java/com/javadub1/organizer/mateuszmedonspring/user/UserController.java
@@ -1,6 +1,8 @@
 package com.javadub1.organizer.mateuszmedonspring.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,10 +10,23 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UserController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/users")
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(path = "/users")
     public ModelAndView hello() {
         ModelAndView modelAndView = new ModelAndView("hello");
         modelAndView.addObject("message","Hello Mateusz");
+        return modelAndView;
+    }
+
+    @GetMapping("/users/{userId}")
+    public ModelAndView userById(@PathVariable("userId") Long id){
+        ModelAndView modelAndView = new ModelAndView("user-view");
+        modelAndView.addObject("user", userService.findById(id));
         return modelAndView;
     }
 }
